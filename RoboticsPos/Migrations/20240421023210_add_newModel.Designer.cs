4c8b7f7019075ec1ba80defca5bc9032f258b214
Tursunmamat Ergashev <150344178+Ergashev1511@users.noreply.github.com>
2024-04-21 07:36:21 +0500

    Added  migration

--- RoboticsPos/Migrations/20240421023210_add_newModel.Designer.cs
+++ RoboticsPos/Migrations/20240421023210_add_newModel.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RoboticsPos.Data.Repositories;
 
@@ -10,9 +11,11 @@ using RoboticsPos.Data.Repositories;
 namespace RoboticsPos.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240421023210_add_newModel")]
+    partial class add_newModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.3");
